88d057378f9e83bba1faac00f80c51b080b2f755
Vitaly Telichko <vitaly.telichko@dataart.com>
2018-04-18 12:35:12 +0300

    added bubblesort

--- sorting_algorithms/bubblesort.js
+++ sorting_algorithms/bubblesort.js
@@ -1,5 +1,12 @@
+/* Bubble sort algorithm
+*  Complexity: O(N^2).
+*  @param Array - as input
+*  @return Array - sorted
+*/
+
 var arr = [3, 2, 5, 7, 8, 4, 1, 0, 9, 6], size = arr.length, i, j, temp;
 
+// 1. Simple example with variable
 for(i = 0; i < size; i += 1) {
     for(j = 0; j < size; j += 1) {
         if(arr[j - 1] > arr[j]) {
@@ -10,4 +17,45 @@ for(i = 0; i < size; i += 1) {
     }
 }
 
-console.log(arr);
+// 2. Advanced example with variable
+for (i = 0; i < size; i += 1) {
+    for (j = i; j > 0; j -= 1) {
+        if ((arr[j] - arr[j - 1]) < 0) {
+            temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+        }
+    }
+}
+
+// 3. Example with swap function (without variable)
+function swap(a, b, arr) {
+    arr[b] = arr[b] - arr[a];
+    arr[a] = arr[a] + arr[b];
+    arr[b] = arr[a] - arr[b];
+}
+
+for(i = 0; i < size; i += 1) {
+    for(j = 0; j < size; j += 1) {
+        if(arr[j - 1] > arr[j]) {
+            swap(j - 1, j, arr);
+        }
+    }
+}
+
+// More concise solution
+function swap(a, arr) {
+    arr[a - 1] = arr[a - 1] - arr[a];
+    arr[a] = arr[a] + arr[a - 1];
+    arr[a - 1] = arr[a] - arr[a - 1];
+}
+
+for(i = 0; i < size; i += 1) {
+    for(j = 0; j < size; j += 1) {
+        if(arr[j - 1] > arr[j]) {
+            swap(j, arr);
+        }
+    }
+}
+
+// console.log(arr);
