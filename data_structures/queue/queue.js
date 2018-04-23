aaae42cabceddabf46e8b0fc97d10d810ee7aeed
Vitaly Telichko <vitaly.telichko@dataart.com>
2018-04-23 18:22:04 +0300

    added stack and queue data structures

---
+++ data_structures/queue/queue.js
@@ -0,0 +1,29 @@
+function Queue() {
+    this.queue = [];
+}
+
+Queue.prototype.enqueue = function(value) {
+    this.queue.push(value);
+};
+
+Queue.prototype.dequeue = function() {
+    return this.queue.shift();
+};
+
+Queue.prototype.peek = function() {
+    return this.queue[0];
+};
+
+Queue.prototype.length = function() {
+    return this.queue.length;
+};
+
+Queue.prototype.print = function() {
+    console.log(this.queue.join(' '));
+};
+
+var queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.print(); // => 1 2 3
