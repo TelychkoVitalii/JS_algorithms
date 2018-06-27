380304a09b037072dd2b65177bc899babcecebcb
Telychko Vitalii <newme1989.5@gmail.com>
2018-06-27 14:28:44 +0300

    added linked list data structure

--- data_structures/queue/queue-es6.js
+++ data_structures/queue/queue-es6.js
@@ -1,28 +1,28 @@
-// class Queue {
-//     constructor() {
-//         this.queue = [];
-//     }
-//
-//     enqueue(value) {
-//         this.queue.push(value);
-//     }
-//
-//     dequeue() {
-//         return this.queue.shift();
-//     }
-//
-//     peek() {
-//         return this.queue[0];
-//     }
-//
-//     length() {
-//         return this.queue.length;
-//     }
-//
-//     print() {
-//         console.log(this.queue.join(' '));
-//     }
-// }
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+
+    enqueue(value) {
+        this.queue.push(value);
+    }
+
+    dequeue() {
+        return this.queue.shift();
+    }
+
+    peek() {
+        return this.queue[0];
+    }
+
+    length() {
+        return this.queue.length;
+    }
+
+    print() {
+        console.log(this.queue.join(' '));
+    }
+}
 //
 // const queue = new Queue();
 // queue.enqueue(1);
--- data_structures/stack/stack-es6.js
+++ data_structures/stack/stack-es6.js
@@ -1,29 +1,29 @@
-// class Stack {
-//     constructor() {
-//         this.stack = [];
-//     }
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(value) {
+        this.stack.push(value);
+    }
+
+    pop() {
+        return this.stack.pop();
+    }
+
+    peek() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    length() {
+        return this.stack.length;
+    }
+
+    print() {
+        console.log(this.stack.join(' '));
+    }
+}
 //
-//     push(value) {
-//         this.stack.push(value);
-//     }
-//
-//     pop() {
-//         return this.stack.pop();
-//     }
-//
-//     peek() {
-//         return this.stack[this.stack.length - 1];
-//     }
-//
-//     length() {
-//         return this.stack.length;
-//     }
-//
-//     print() {
-//         console.log(this.stack.join(' '));
-//     }
-// }
-// //
 // // const stack = new Stack();
 // // stack.push(1);
 // // stack.push(2);
