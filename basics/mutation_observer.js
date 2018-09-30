31d3248099daf375d515ce3b33ba3c256d44f357
Vitalii Telychko <vtel@ciklum.com>
2018-09-30 15:34:28 +0300

    added mutation observer

---
+++ basics/mutation_observer.js
@@ -0,0 +1,13 @@
+// const target = document.getElementById('some-id'),
+//       span = document.createElement('span'),
+//       observer = new MutationObserver(mutations => mutations.forEach(mutation => console.log(mutation.type))),
+//       config = { attributes: true, childList: true, characterData: true };
+//
+// observer.observe(target, config);
+//
+// // Change childList mutation type
+// span.innerText = 'Fuck';
+// target.appendChild(span);
+//
+// // Change attributes mutation type
+// target.id = 'id';
