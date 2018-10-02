0bc8ff68817db74f752630d15df4af47beb410f4
Vitalii Telychko <vtel@ciklum.com>
2018-10-02 11:43:44 +0300

    Catching Car Mileage Numbers

---
+++ codewars/junior_level/validate_sudoku_with_size_NxN.js
@@ -0,0 +1,16 @@
+var Sudoku = function(data) {
+  //   Private methods
+  // -------------------------
+
+
+
+  //   Public methods
+  // -------------------------
+  return {
+    isValid: function() {
+      // YOUR SOLUTION
+
+      return true;
+    }
+  };
+};
--- design_patterns/structural/Decorator.js
+++ design_patterns/structural/Decorator.js
@@ -23,7 +23,6 @@ var DecoratedUser = function (user, street, city) {
 };
 
 // logging helper
-
 var log = (function() {
     var log = "";
 
